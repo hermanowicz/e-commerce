45e4fd8c18f2eb61a89432cb47d9b3f5f35c0d12
Tomasz Hermanowicz <hermangti@gmail.com>
2025-04-17 17:40:38 +0200

    removed views I will go with next.js added base of application deps air aka nodemon for go works fine

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var (
+	port int = 12345
 )
 
 func main() {
-	fmt.Println("Hello, World")
+	fmt.Println("Hello, E-commerce admin starting app at port:", port)
+
+	r := chi.NewRouter()
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.StripSlashes)
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World"))
+	})
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		Handler:      r,
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Fatal("startup failed with error:", err.Error())
+	}
 }
